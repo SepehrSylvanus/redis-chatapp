65a8420a2890e61fcc6b617233a63bd3e657cea3
Sylvanus <sepehr.haghdoust@gmail.com>
2024-06-29 13:43:11 +0330

    implementing auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return <div className="text-red-500">Hello world</div>;
+  return <Button variant={"ghost"}>Hello</Button>;
 }
